632e0dddbc905daec15932f73d2abcc8a1c631c5
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-05-08 16:07:09 -0400

    Initial commit

---
+++ data.R
@@ -0,0 +1,12 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(0)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
